e38d2bde93e065bf3ddf8ed69a60443a08a2bf47
K <waterskm2013@gmail.com>
2017-11-13 17:32:28 -0500

    added comments and formatted tables

--- Documents/show-me-the-money/src/App.js
+++ Documents/show-me-the-money/src/App.js
@@ -2,24 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Fields to capture User input (including Submit button)
 const SubmitForm = (
   { handleFormSubmit, formValueName, formValueCategory, formValueAmount, 
     handleFormChangeName, handleFormChangeCategory, handleFormChangeAmount, 
     labelName, labelCategory, labelAmount, 
-    buttonStyle }) => (
-  <div>
-    {labelName}: <input type="text"  style={{ height: '20px', marginRight: '5px'}} onChange={handleFormChangeName} value={formValueName} />
-    {labelCategory}: <input type="text" style={{ height: '20px', marginRight: '5px' }} onChange={handleFormChangeCategory} value={formValueCategory} />
-    {labelAmount} : <input type="text" style={{ height: '20px', marginRight: '5px' }} onChange={handleFormChangeAmount} value={formValueAmount} />
-    <span style={buttonStyle} onClick={handleFormSubmit}>Submit</span>
-  </div> 
+    buttonStyle
+  }) => (
+    <div>
+      {labelName}: <input type="text"  style={{ height: '20px', marginRight: '5px'}} onChange={handleFormChangeName} value={formValueName} />
+      {labelCategory}: <input type="text" style={{ height: '20px', marginRight: '5px' }} onChange={handleFormChangeCategory} value={formValueCategory} />
+      {labelAmount} : <input type="text" style={{ height: '20px', marginRight: '5px' }} onChange={handleFormChangeAmount} value={formValueAmount} />
+      <span style={buttonStyle} onClick={handleFormSubmit}>Submit</span>
+    </div> 
 )
 
+// Table component maps values entered into a table to show records
 const Table = ({ values }) => (
   <div>
     {values.map(function(currentValue, index) {
-      return(<div style={{ align: "left"}} key={index}>
-        {currentValue.name}        
+      return(<div style={{ align: "left", marginRight:'5px'}} key={index}>
+        <div style={{ width: '300px', textAlign: 'left' }}>
+          <div style={{ width: '100px', display: 'inline-block' }}>{currentValue.name}</div>
+          <div style={{ width: '100px', display: 'inline-block' }}>{currentValue.category}</div>
+          <div style={{ width: '100px', display: 'inline-block' }}>{currentValue.amount}</div>
+        </div>
       </div>)
     })}
   </div>
@@ -29,14 +36,15 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    // Various state items needed throughout code
     this.state = {
-      textValue: '',
-      formValueName: '',
-      formValueCategory: '',
-      formValueAmount: '',
-      submittedValues: []
+      formValueName: '',      // Name
+      formValueCategory: '',  // Category
+      formValueAmount: '',    // Amount
+      submittedValues: []     // All of the above, used to easily capture all values and pass to Table
     };
 
+    // Bindings
     this.handleInput = this.handleInput.bind(this);
     this.handleFormChangeName = this.handleFormChangeName.bind(this);
     this.handleFormChangeCategory = this.handleFormChangeCategory.bind(this);
@@ -44,33 +52,33 @@ class App extends Component {
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  handleInput(e) {
+  // Handling functions
+  handleInput(e) {  // Adjusts the state
     this.setState({ textValue: e.target.value });
   }
 
-  handleFormSubmit(e) {
-    const newState = this.state.submittedValues;
-    newState.push({ name: this.state.formValueName, category: this.state.formValueCategory, amount:this.state.formValueAmount });
-    this.setState({ submittedValues: newState, formValueName: '', formValueCategory: '', formValueAmount: '' });
+  handleFormSubmit(e) { // When Submit is clicked
+    const newState = this.state.submittedValues; // Create a new state (empty array)
+    newState.push({ name: this.state.formValueName, category: this.state.formValueCategory, amount:this.state.formValueAmount }); // Push data entered by user into the new state array
+    this.setState({ submittedValues: newState, formValueName: '', formValueCategory: '', formValueAmount: '' }); // Reset submittedValues to be empty for next set of data
   }
 
-  handleFormChangeName(e) {
+  handleFormChangeName(e) { // When data is entered in Name field, save somewhere
     this.setState({ formValueName: e.target.value });
   }
 
-  handleFormChangeCategory(e) {
+  handleFormChangeCategory(e) { // When data is entered in Category field, save somewhere
     this.setState({ formValueCategory: e.target.value });
   }
 
-  handleFormChangeAmount(e) {
+  handleFormChangeAmount(e) { // When data is entered in Amount field save somewhere
     this.setState({ formValueAmount: e.target.value });
   }
 
   render() {
     return (
-      <div className="App">
-        <div style={{ fontSize: '75px', textAlign: 'center' }}>Show Me the Money!</div>
-        <Table values={this.state.submittedValues} />
+      <div className="App">8
+        <div style={{ fontSize: '75px', textAlign: 'center' }}>Show Me the Money!</div> { /* Title */ }
         <SubmitForm
           handleFormSubmit={this.handleFormSubmit}
           formValueName={this.state.formValueName}
@@ -84,6 +92,12 @@ class App extends Component {
           labelAmount="Amount ($)"
           buttonStyle={{ backgroundColor: 'blue', color: 'white', height: '20px', padding: '3px 10px', borderRadius: '4px' }}
         />
+        <div style={{ width: '300px', textAlign: 'left' }}>
+          <div style={{ fontWeight: 'bold', width: '100px', display: 'inline-block' }}>Name</div>
+          <div style={{ fontWeight: 'bold', width: '100px', display: 'inline-block' }}>Category</div>
+          <div style={{ fontWeight: 'bold', width: '100px', display: 'inline-block' }}>Amount</div>
+        </div>
+        <Table values={this.state.submittedValues} /> { /* Values submitted by User appear in this table. */ }
       </div>
     );
   }
